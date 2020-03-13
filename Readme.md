d58bfb4aae6e446ca769c65ad6d34e04b25c8c98
Dominik <dominik.otto@izi.fraunhofer.de>
2020-03-13 16:26:54 +0100

    project structure

---
+++ Readme.md
@@ -0,0 +1,2 @@
+This project supllies a utility to quickly tag studies and samples from
+the [NCBI Gene Expression Omnibus](https://www.ncbi.nlm.nih.gov/geo/).
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+    setuptools.setup(
+        name="geotag",
+        version="0.0.1",
+        author="Dominik Otto",
+        author_email="dominik.otto@gmail.com",
+        description="Python tool to tag samples from GEO.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://ribogit.izi.fraunhofer.de/Dominik/geotag",
+        packages=setuptools.find_packages(),
+        classifiers=[
+                    "Programming Language :: Python :: 3",
+                    "License :: OSI Approved :: GNUv3 License",
+                    "Operating System :: OS Independent",
+                ],
+        python_requires='>=3.6',
+    )
